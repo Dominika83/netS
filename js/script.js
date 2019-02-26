31fa98e544c6a465f9f8087aa2970177adcd3f1a
DSOBIECKA-TESLA\dsobiecka <dominika.sobiecka@mobi-me.pl>
2019-02-26 17:02:47 +0100

    rwd: first changes

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
-
+//Slick JS - carousel product
 $('.slider-for').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
     fade: true,
-    asNavFor: '.slider-nav'
+    asNavFor: '.slider-nav',
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: true,
+            }
+        },
+    ]
 });
 $('.slider-nav').slick({
     slidesToShow: 4,
@@ -12,4 +22,41 @@ $('.slider-nav').slick({
     asNavFor: '.slider-for',
     dots: false,
     focusOnSelect: true
+
 });
+
+//Smooth Scrolling
+$('a[href*="#"]')
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function (event) {
+        if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+            &&
+            location.hostname == this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000, function () {
+                    // Callback after animation
+                    // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) { // Checking if the target was focused
+                        return false;
+                    } else {
+                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                        $target.focus(); // Set focus again
+                    }
+                    ;
+                });
+            }
+        }
+    });
+
+Resources
